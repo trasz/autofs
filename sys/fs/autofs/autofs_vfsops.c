2ede9d3632dc5e00026ddd99ea33ebad39cfa725
Edward Tomasz Napierala <trasz@FreeBSD.org>
2013-12-31 10:35:27 +0100

    device

--- sys/fs/autofs/autofs_vfsops.c
+++ sys/fs/autofs/autofs_vfsops.c
@@ -36,13 +36,43 @@
 #include <sys/kernel.h>
 #include <sys/module.h>
 #include <sys/mount.h>
+#include <sys/sysctl.h>
 #include <sys/vnode.h>
+
+static MALLOC_DEFINE(M_AUTOFS, "autofs", "Automounter filesystem");
+
+#define	AUTOFS_DEBUG(X, ...)					\
+	if (debug > 1) {					\
+		printf("%s: " X "\n", __func__, ## __VA_ARGS__);\
+	} while (0)
+
+#define	AUTOFS_WARN(X, ...)					\
+	if (debug > 0) {					\
+		printf("WARNING: %s: " X "\n",			\
+		    __func__, ## __VA_ARGS__);			\
+	} while (0)
 
 static const char *autofs_opts[] = {
 	"export", "from", NULL
 };
 
-static struct vnode *rootvp;
+static int	autofs_ioctl(struct cdev *dev, u_long cmd, caddr_t arg,
+		    int mode, struct thread *td);
+
+static struct cdevsw autofs_cdevsw = {
+     .d_version = D_VERSION,
+     .d_ioctl   = autofs_ioctl,
+     .d_name    = "autofs",
+};
+
+static struct autofs_softc	*sc;
+static struct vnode		*rootvp;
+
+SYSCTL_NODE(_vfs, OID_AUTO, autofs, CTLFLAG_RD, 0, "Automounter filesystem");
+static int debug = 2;
+TUNABLE_INT("vfs.autofs.debug", &debug);
+SYSCTL_INT(_vfs_autofs, OID_AUTO, debug, CTLFLAG_RW,
+    &debug, 2, "Enable debug messages");
 
 int	autofs_rootvp(struct mount *mp, struct vnode **vpp);
 
@@ -64,8 +94,6 @@ autofs_mount(struct mount *mp)
 	error = autofs_rootvp(mp, &rootvp);
 	if (error != 0)
 		return (error);
-
-	printf("rootvp is %p\n", rootvp);
 
 	vfs_mountedfrom(mp, from);
 
@@ -128,12 +156,65 @@ autofs_statfs(struct mount *mp, struct statfs *sbp)
 	return (0);
 }
 
+struct autofs_softc {
+	device_t			sc_dev;
+	struct cdev			*sc_cdev;
+};
+
+static int
+autofs_init(struct vfsconf *vfsp)
+{
+	int error;
+
+	sc = malloc(sizeof(*sc), M_AUTOFS, M_ZERO | M_WAITOK);
+
+	error = make_dev_p(MAKEDEV_CHECKNAME, &sc->sc_cdev, &autofs_cdevsw,
+	    NULL, UID_ROOT, GID_WHEEL, 0600, "autofs");
+	if (error != 0) {
+		AUTOFS_WARN("failed to create device node, error %d", error);
+		return (error);
+	}
+	sc->sc_cdev->si_drv1 = sc;
+
+	return (0);
+}
+
+static int
+autofs_uninit(struct vfsconf *vfsp)
+{
+
+	if (sc->sc_cdev != NULL) {
+		AUTOFS_DEBUG("removing device node");
+		destroy_dev(sc->sc_cdev);
+		AUTOFS_DEBUG("device node removed");
+	}
+
+	free(sc, M_AUTOFS);
+	return (0);
+}
+
+static int
+autofs_ioctl(struct cdev *dev, u_long cmd, caddr_t arg, int mode,
+    struct thread *td)
+{
+	struct autofs_softc *sc;
+
+	sc = dev->si_drv1;
+
+	switch (cmd) {
+	default:
+		return (EINVAL);
+	}
+}
+
 static struct vfsops autofs_vfsops = {
 	.vfs_fhtovp =		NULL, /* XXX */
 	.vfs_mount =		autofs_mount,
+	.vfs_unmount =		autofs_unmount,
 	.vfs_root =		autofs_root,
 	.vfs_statfs =		autofs_statfs,
-	.vfs_unmount =		autofs_unmount,
+	.vfs_init =		autofs_init,
+	.vfs_uninit =		autofs_uninit,
 };
 
 VFS_SET(autofs_vfsops, autofs, VFCF_SYNTHETIC | VFCF_NETWORK);
