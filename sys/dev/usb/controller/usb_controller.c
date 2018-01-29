b289bff6d46f177c7f93bd8fa73f3fe380a2ffe3
Edward Tomasz Napierala <trasz@FreeBSD.org>
2018-01-29 09:36:41 +0000

    Revert "Remove debugging printfs."
    This reverts commit 148445b7a5d5ac61faaee007ad2047fc6bdff798.

--- sys/dev/usb/controller/usb_controller.c
+++ sys/dev/usb/controller/usb_controller.c
@@ -649,6 +649,8 @@ usb_bus_pull_up(struct usb_proc_msg *pm)
 	struct usb_device *udev;
 	usb_error_t err;
 
+	printf("%s: go!\n", __func__);
+
 	bus = ((struct usb_bus_msg *)pm)->bus;
 	udev = bus->devices[USB_ROOT_HUB_ADDR];
 
@@ -660,6 +662,7 @@ usb_bus_pull_up(struct usb_proc_msg *pm)
 		DPRINTF("usbd_req_set_port_feature() failed: %s\n",
 		    usbd_errstr(err));
 	}
+	printf("%s: done\n", __func__);
 }
 
 static void
@@ -668,6 +671,8 @@ usb_bus_pull_down(struct usb_proc_msg *pm)
 	struct usb_bus *bus;
 	struct usb_device *udev;
 	usb_error_t err;
+
+	printf("%s: go!\n", __func__);
 
 	bus = ((struct usb_bus_msg *)pm)->bus;
 	udev = bus->devices[USB_ROOT_HUB_ADDR];
@@ -680,6 +685,7 @@ usb_bus_pull_down(struct usb_proc_msg *pm)
 		DPRINTF("usbd_req_set_port_feature() failed: %s\n",
 		    usbd_errstr(err));
 	}
+	printf("%s: done\n", __func__);
 }
 
 /*------------------------------------------------------------------------*
